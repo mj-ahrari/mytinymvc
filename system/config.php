62940b9ec8f85890ecd21119a4936bc1f8a287aa
mj00770 <ahrari.eh@gmail.com>
2020-12-12 10:05:55 +0330

    database information added to config.php file

--- system/config.php
+++ system/config.php
@@ -2,4 +2,8 @@
 $protocol = (strpos($_SERVER['SERVER_PROTOCOL'],"https"))? "https":"http";
 define("BASE_URL",$protocol."://".$_SERVER['HTTP_HOST']."/tinymvc/");
 define("ROOT",__DIR__."/../");
+define("HOST","localhost");
+define("DBNANE","tinymvc");
+define("USERNAME","root");
+define("PASSWORD","");
 ?>
